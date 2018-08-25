2a9dc0d84c5b911d093e6646296a9170bfc1a4b6
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-25 14:39:07 +0200

    Add state to search page to behaves correctly

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react'
 
 class SearchPage extends Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({query})
+    }
+
     render() {
         return(
             <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                <div className="search-books-input-wrapper">
+                
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author"
+                    value={this.state.query}
+                    onChange={(e) =>
+                        this.updateQuery(e.target.value)
+                    }
+                />
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
+                </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+                <ol className="books-grid"></ol>
             </div>
-          </div>
+            </div>
         )
     }
-}
+    }
 
 export default SearchPage
